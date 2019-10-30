8f3d7b8963f2f055188a1b6aaaeb6c0f8d8bfbec
tianyu1 <18222170921@163.com>
2019-10-30 16:07:04 +0800

    learn route-props增加注释

--- src/router/index.js
+++ src/router/index.js
@@ -2,39 +2,40 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Hello from '../views/Hello'
 
-Vue.use(VueRouter)
-function dynamicPropsFn (route) {
-  const now = new Date()
-  return {
-    name: (now.getFullYear() + parseInt(route.params.years)) + '!'
-  }
+Vue.use(VueRouter);
+
+function dynamicPropsFn(route) {
+    const now = new Date();
+    return {
+        name: (now.getFullYear() + parseInt(route.params['years'])) + '!'
+    }
 }
 
 const routes = [
-  /*{
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/!* webpackChunkName: "about" *!/ '../views/About.vue')
-  }*/
-  { path: '/', component: Hello }, // No props, no nothing
-  { path: '/hello/:name', component: Hello, props: true }, // Pass route.params to props,布尔类型
-  { path: '/static', component: Hello, props: { name: 'world' }}, // static values，对象类型
-  { path: '/dynamic/:years', component: Hello, props: dynamicPropsFn }, // custom logic for mapping between route and props，函数类型
-  { path: '/attrs', component: Hello, props: { name: 'attrs' }}//对象类型
-]
+    /*{
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/!* webpackChunkName: "about" *!/ '../views/About.vue')
+    }*/
+    {path: '/', component: Hello}, // No props, no nothing
+    {path: '/hello/:name', component: Hello, props: true}, // Pass route.params to props,布尔类型
+    {path: '/static', component: Hello, props: {name: 'world'}}, // static values，对象类型
+    {path: '/dynamic/:years', component: Hello, props: dynamicPropsFn}, // custom logic for mapping between route and props，函数类型
+    {path: '/attrs', component: Hello, props: {name: 'attrs'}}//对象类型
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+});
 
 export default router
