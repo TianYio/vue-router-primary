2a70dd49ededf7821520282f97a45e38c03741ca
tianyu1 <18222170921@163.com>
2019-10-30 15:57:18 +0800

    learn route-props

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Hello from '../views/Hello'
 
 Vue.use(VueRouter)
+function dynamicPropsFn (route) {
+  const now = new Date()
+  return {
+    name: (now.getFullYear() + parseInt(route.params.years)) + '!'
+  }
+}
 
 const routes = [
-  {
+  /*{
     path: '/',
     name: 'home',
     component: Home
@@ -16,8 +22,13 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import(/!* webpackChunkName: "about" *!/ '../views/About.vue')
+  }*/
+  { path: '/', component: Hello }, // No props, no nothing
+  { path: '/hello/:name', component: Hello, props: true }, // Pass route.params to props
+  { path: '/static', component: Hello, props: { name: 'world' }}, // static values
+  { path: '/dynamic/:years', component: Hello, props: dynamicPropsFn }, // custom logic for mapping between route and props
+  { path: '/attrs', component: Hello, props: { name: 'attrs' }}
 ]
 
 const router = new VueRouter({
