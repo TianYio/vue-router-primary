580094b6be658a32dfc5f4d7c8d6553233b47e71
tianyu1 <18222170921@163.com>
2019-11-01 17:08:53 +0800

    learn vue model-自定义组件的v-model

--- src/router/router.js
+++ src/router/router.js
@@ -14,30 +14,49 @@ function dynamicPropsFn(route) {
 
 const routes = [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+        path: '/',
+        name: 'home',
+        component: Home
+    },// No props, no nothing
+    {
+        path: '/hello/:name',
+        component: Home,
+        props: true
+    }, // Pass route.params to props,布尔类型
+    {
+        path: '/static',
+        component: Home,
+        props: {name: 'world'}
+    }, // static values，对象类型
+    {
+        path: '/dynamic/:years',
+        component: Home,
+        props: dynamicPropsFn
+    }, // custom logic for mapping between route and props，函数类型
+    {
+        path: '/attrs',
+        component: Home,
+        props: {name: 'attrs'}
+    },//对象类型
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
     },
-    /*{
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/!* webpackChunkName: "about" *!/ '../views/About.vue')
-    }*/
-    {path: '/', component: Hello}, // No props, no nothing
-    {path: '/hello/:name', component: Hello, props: true}, // Pass route.params to props,布尔类型
-    {path: '/static', component: Hello, props: {name: 'world'}}, // static values，对象类型
-    {path: '/dynamic/:years', component: Hello, props: dynamicPropsFn}, // custom logic for mapping between route and props，函数类型
-    {path: '/attrs', component: Hello, props: {name: 'attrs'}}//对象类型
+    {
+        path: '/model',
+        component: () => import('@/views/model.vue')
+    }
 ];
 
 const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes,
-    linkActiveClass:'router-link-active'
+    linkActiveClass: 'router-link-active'
 });
 
 export default router
---
+++ src/views/model.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        model:{{model}}
+        <ModelComponent v-model="model"></ModelComponent>
+    </div>
+</template>
+<script>
+    import ModelComponent from "../components/ModelComponent";
+    export default {
+        name:'model',
+        components:{
+            ModelComponent
+        },
+        data(){
+            return {
+                model:true
+            }
+        }
+
+    }
+</script>
+<style scoped >
+
+</style>
