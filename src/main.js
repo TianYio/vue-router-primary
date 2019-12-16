e8b911bd973b4eb8b37c8706e78f0cafeba35b04
TianYio <1711839211@qq.com>
2019-12-16 18:06:20 +0800

    引入 ant-design

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 import {Demo} from "./directives/directives";
 import {IsTrue} from "./filters/filter";
 
-Vue.config.performance = true;
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 /*Vue.directive('demo', {
     bind: function (el, {name, value, oldValue, expression, arg, modifiers}, vnode, oldVnode) {
         let s = JSON.stringify;
@@ -21,5 +20,5 @@ Vue.use(Demo);
 Vue.use(IsTrue);
 new Vue({
     router,
-    render: h => h(App)
-}).$mount('#app')
+    render: h => h(App),
+}).$mount('#app');
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    css: {
+        loaderOptions: {
+            less: {
+                modifyVars: {
+                    'primary-color': '#EA8004'
+                },
+                javascriptEnabled: true
+            }
+        }
+    }
+};
